9cd5de7302cfcbb9ff6b9c1d98837028ad5b34e3
Bikra123 <bikraabn@gmail.com>
2024-04-04 06:38:47 +0700

    update

---
+++ js/js-event.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById('btn')
+btn.addEventListener('click',()=>{
+    console.log("Hello World")
+})
+
+//ada berbagai macam event di javascript dan ini adalah sebuah contoh ketika ada 
+//sebuah element yang diberi id button di klik, akan memunculkan teks Hello World di console
+//anda juga bisa menambahkan atribut event semisal onclick pada elemen html
+//dan menambahkan sebuah fungsi yang sudah di buat di javascript
+//contoh <button onclick="hello">Klik Here</button>
+ function hello(){
+    console.log("Hello World")
+ }
+
+// akan memunculkan teks Hello World di console
