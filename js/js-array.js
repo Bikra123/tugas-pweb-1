66700a2352997db6c04496548f245c6bf2e46f3b
bikraabna123 <bikraabn@gmail.com>
2024-04-04 11:53:19 +0700

    main

---
+++ js/js-array.js
@@ -0,0 +1,6 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+console.log(fruits.length)//jumlah data dalam array
+console.log(fruits.toString())//mengkonversi data ke dalam string dan dipisahkan dengan koma
+console.log(fruits.at(1))//menampilkan buah dengan index ke 1 
+console.log(fruits[1])//menampilkan buah dengan index ke 1 dengan []
